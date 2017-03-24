123cd052c7201bf1c5fdb0f056b5f02860f439c9
Florian Göbel <florian.goebel@linkit.tech>
2017-03-24 16:31:00 +0100

    Added the class “blogLeft” (This is our blog site)

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
           <div id="content">
             <div class="container">
               <div class="row">
-                <div class="col-sm-8">
+                <div class="blogLeft col-sm-8">
                   <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
                     <div class="article">
                       <h2 class="title"><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
