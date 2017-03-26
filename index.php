8416fbd92aae4a4558bd54803a4e98eb10191298
Florian Göbel <florian.goebel@linkit.tech>
2017-03-26 22:06:12 +0200

    Added the blue icons and the information below the headline of our blog sites.

--- index.php
+++ index.php
@@ -8,6 +8,12 @@
                   <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
                     <div class="article">
                       <h2 class="title"><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
+                      <div class="blog-header"></div>
+                      <p>
+                        <i style="color: #64B4DC;" class="fa fa-clock-o"></i> <?php the_date('d.m.Y'); ?> <i style="margin-left: 20px; color: #64B4DC;" class="fa fa-pencil" aria-hidden="true"></i> <?php the_author(); ?>
+                          <i style="margin-left: 20px; color: #64B4DC;" class="fa fa-folder-open-o"></i>
+                            <?php the_category(', '); ?>
+                      </p>
                       <?php the_content(); ?>
                     </div> <!-- .article -->
                   <?php endwhile; endif; ?>
--- single.php
+++ single.php
@@ -2,16 +2,14 @@
   <div id="main">
     <div class="container">
       <div class="row">
-        <div class="col-sm-8">
+        <div style="margin-top: 5vh;" class="col-sm-8">
           <div id="content">
             <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
               <h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
-
-              <div id="meta">
-          <span>geschrieben am: <?php the_date('d.m.Y'); ?>
-          von: <?php the_author(); ?> in
-          Kategorie(n): <?php the_category(', '); ?><?php the_tags(' und getagged mit: ', '', ''); ?></span>
-              </div>
+              <div class="blog-header"></div>
+              <p><i style="color: #64B4DC;" class="fa fa-clock-o"></i> <?php the_date('d.m.Y'); ?> <i style="margin-left: 20px; color: #64B4DC;" class="fa fa-pencil" aria-hidden="true"></i> <?php the_author(); ?>
+                <i style="margin-left: 20px; color: #64B4DC;" class="fa fa-folder-open-o"></i>
+                  <?php the_category(', '); ?> </p>
 
               <div class="entry">
           <?php the_content(); ?>
